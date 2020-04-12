5c3b4739342b5c1a1fc2888f65d89e40ec63d5c8
srivishvae <srivishvaelango@gmail.com>
2020-04-12 10:28:09 +0530

    Azure deployment made in settings.py

--- Helphen_India/settings.py
+++ Helphen_India/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x^i(1=uv$0k5k&ld6__2(tdiq+r9h--c87yw*6^s7yar#1^jl+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
